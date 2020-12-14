ce854042d15a4ab1ab0872e94e949030af956b58
Елена Романова <elena.romanova.1999@mail.ru>
2020-12-14 22:16:53 +0700

    Fix exit prog with close connection; Add get groups and his msgs

--- K_Client/Form2.cs
+++ K_Client/Form2.cs
@@ -13,9 +13,11 @@ namespace K_Client
     public partial class Form2 : Form
     {
         private List<string> _chts = null;
+        private List<string> _msgs = null;
 
         public void global_FormClosed(object sender, EventArgs e)
         {
+            TCP_Client_K.Send("BYE\r\n\r\n");
             Application.Exit();
         }
 
@@ -27,8 +29,25 @@ namespace K_Client
             TCP_Client_K.Send("GET CHATS\r\n\r\n");
             var res = TCP_Client_K.Read();
             _chts = res.Split(';').ToList();
-            _chts.Remove("");
+            _chts.Remove(""); //fix last empty obj
 
+            listBox_groups.DataSource = _chts;
+
+            listBox_groups.SelectedIndexChanged += ListBox_groups_SelectedIndexChanged;
+        }
+
+        private void ListBox_groups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_messages.Text = "";
+            TCP_Client_K.Send("GET MSG " + listBox_groups.SelectedItem.ToString() + "\r\n\r\n");
+            var res = TCP_Client_K.Read();
+            _msgs = res.Split(';').ToList();
+            _msgs.Remove(""); //fix last empty obj
+
+            foreach(var str in _msgs)
+            {
+                textBox_messages.Text += str + "\r\n";
+            }
         }
     }
 }
