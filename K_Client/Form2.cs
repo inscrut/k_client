d1db2380b0fa7c029ddce5482ec3cd90d3322ecc
Елена Романова <elena.romanova.1999@mail.ru>
2020-12-15 20:12:53 +0700

    Changed SEND CHAT cmd; well working;

--- K_Client/Form2.cs
+++ K_Client/Form2.cs
@@ -34,9 +34,36 @@ namespace K_Client
             listBox_groups.DataSource = _chts;
 
             listBox_groups.SelectedIndexChanged += ListBox_groups_SelectedIndexChanged;
+            button_send.Click += Button_send_Click;
+        }
+
+        private void Button_send_Click(object sender, EventArgs e)
+        {
+            if(textBox_msg.TextLength <= 0)
+            {
+                MessageBox.Show("Необходимо ввести текст сообщения!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            TCP_Client_K.Send("SEND CHAT " + listBox_groups.SelectedItem.ToString() + " " + textBox_msg.Text + "\r\n\r\n");
+            var res = TCP_Client_K.Read();
+            if (res.Contains("OK"))
+            {
+                textBox_msg.Text = "";
+                update_msgs();
+            }
+            else
+            {
+                MessageBox.Show("Произошла ошибка отправки сообщения!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
         }
 
         private void ListBox_groups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update_msgs();
+        }
+
+        private void update_msgs()
         {
             textBox_messages.Text = "";
             TCP_Client_K.Send("GET MSG " + listBox_groups.SelectedItem.ToString() + "\r\n\r\n");
@@ -44,7 +71,7 @@ namespace K_Client
             _msgs = res.Split(';').ToList();
             _msgs.Remove(""); //fix last empty obj
 
-            foreach(var str in _msgs)
+            foreach (var str in _msgs)
             {
                 textBox_messages.Text += str + "\r\n";
             }
